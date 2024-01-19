fb0333b2c29966e187a00bc8074ed4ba4fdf87a7
Mario Tormo <mario@tormo-romero.eu>
2024-01-19 11:20:29 +0100

    Add files via upload

---
+++ notebooks/nb_config.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('..')
+
+DATA_PATH = r'../data/'
+RAW_DATA_PATH = DATA_PATH + r'raw/'
+EXTERNAL_DATA_PATH = DATA_PATH + r'external/'
+INTERIM_DATA_PATH = DATA_PATH + r'interim/'
+PROCESSED_DATA_PATH = DATA_PATH + r'processed/'
